fbd78b9c129cefb1ac1c454584d98003ca2d820f
petrostrak <pit.trak@gmail.com>
2022-04-11 18:25:11 +0300

    Generate GetTransfer with sqlc

--- db/sqlc/transfer.sql.go
+++ db/sqlc/transfer.sql.go
@@ -37,3 +37,21 @@ func (q *Queries) CreateTransfer(ctx context.Context, arg CreateTransferParams) 
 	)
 	return i, err
 }
+
+const getTransfer = `-- name: GetTransfer :one
+SELECT id, from_account_id, to_account_id, amount, created_at FROM transfers
+WHERE id = $1 LIMIT 1
+`
+
+func (q *Queries) GetTransfer(ctx context.Context, id int64) (Transfer, error) {
+	row := q.db.QueryRowContext(ctx, getTransfer, id)
+	var i Transfer
+	err := row.Scan(
+		&i.ID,
+		&i.FromAccountID,
+		&i.ToAccountID,
+		&i.Amount,
+		&i.CreatedAt,
+	)
+	return i, err
+}
