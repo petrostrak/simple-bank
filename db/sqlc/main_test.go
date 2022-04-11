15a841449ce7e73ae2d3d14012d1a04bdd1cbd81
petrostrak <pit.trak@gmail.com>
2022-04-11 21:47:14 +0300

    Add TestMain

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+)
+
+var testQueries *Queries
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
