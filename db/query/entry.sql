3e4ed13ce3b91e1ac0ac3487c7dde6a2abaf6076
petrostrak <pit.trak@gmail.com>
2022-04-11 18:21:25 +0300

    Generate ListEntries

--- db/query/entry.sql
+++ db/query/entry.sql
@@ -9,3 +9,10 @@ INSERT INTO entries (
 -- name: GetEntry :one
 SELECT * FROM entries
 WHERE id = $1 LIMIT 1;
+
+-- name: ListEntries :many
+SELECT * FROM entries
+WHERE account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3;
