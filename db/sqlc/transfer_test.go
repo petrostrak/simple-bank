300a4a20b32170a6f2d08906e0f248c23cdc5adb
petrostrak <pit.trak@gmail.com>
2022-04-12 19:06:54 +0300

    Add createRandomTransfer

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,30 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/petrostrak/simple-bank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomTransfer(t *testing.T, account1, account2 Account) Transfer {
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        util.RandomMoney(),
+	}
+
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+
+	require.Equal(t, arg.FromAccountID, transfer.FromAccountID)
+	require.Equal(t, arg.ToAccountID, transfer.ToAccountID)
+	require.Equal(t, arg.Amount, transfer.Amount)
+
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+
+	return transfer
+}
