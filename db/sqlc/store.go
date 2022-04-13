0a97c3adf7b17bd7a805c4091487b678dabb9c5c
petrostrak <pit.trak@gmail.com>
2022-04-13 13:11:20 +0300

    Add execTx method

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -1,6 +1,10 @@
 package db
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
 
 // Store provides all functions to execute db queries and transactions
 type Store struct {
@@ -13,4 +17,23 @@ func NewStore(db *sql.DB) *Store {
 		db:      db,
 		Queries: New(db),
 	}
+}
+
+// execTx executes a function within a database transaction
+func (s *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := s.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	if err = fn(q); err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+
+		return err
+	}
+
+	return tx.Commit()
 }
