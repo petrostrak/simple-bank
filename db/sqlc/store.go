802549988e5028cf30b876d5d1d56fc67ed0e090
petrostrak <pit.trak@gmail.com>
2022-04-13 13:06:16 +0300

    Add store struct and NewStore func

---
+++ db/sqlc/store.go
@@ -0,0 +1,16 @@
+package db
+
+import "database/sql"
+
+// Store provides all functions to execute db queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
