1e96f3f2d84f7120da3dfc857409d4f59905b8df
petrostrak <pit.trak@gmail.com>
2022-04-15 13:54:45 +0300

    Use viper config in main_test.go

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+	"github.com/petrostrak/simple-bank/util"
 )
 
 var (
@@ -14,15 +15,13 @@ var (
 	testDB      *sql.DB
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:secret@localhost:5432/simple_bank?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
