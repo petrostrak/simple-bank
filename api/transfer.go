c2bc6a88931d977ea452cf24df4f0ae75de22598
petrostrak <pit.trak@gmail.com>
2022-04-16 10:18:01 +0300

    Add createTransfer handler

---
+++ api/transfer.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/petrostrak/simple-bank/db/sqlc"
+)
+
+type transferRequest struct {
+	FromAccountID int64 `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64 `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64 `json:"amount" binding:"required,gt=0"`
+	Currency      int64 `json:"currency" binding:"required,oneof=RUB YUAN YEN"`
+}
+
+func (s *Server) createTransfer(ctx *gin.Context) {
+	var req transferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.TransferTxParams{
+		FromAccountID: req.FromAccountID,
+		ToAccountID:   req.ToAccountID,
+		Amount:        req.Amount,
+	}
+
+	result, err := s.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, result)
+}
