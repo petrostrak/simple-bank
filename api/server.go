e08dba222a3b01a396c4fb66f876fc6c0b22ef5a
petrostrak <pit.trak@gmail.com>
2022-04-18 17:29:24 +0300

    Update NewServer to generate a new paseto token for login purposes

--- api/server.go
+++ api/server.go
@@ -1,21 +1,37 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/petrostrak/simple-bank/db/sqlc"
+	"github.com/petrostrak/simple-bank/token"
+	"github.com/petrostrak/simple-bank/util"
 )
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(s db.Store) *Server {
-	server := &Server{store: s}
+func NewServer(config util.Config, s db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      s,
+		tokenMaker: tokenMaker,
+	}
+
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -31,7 +47,7 @@ func NewServer(s db.Store) *Server {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
+	return server, nil
 }
 
 // Start runs the HTTP server on a specific address.
