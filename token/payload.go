78e98dc660e1f92c71c6cab8e0150b18b9759efb
petrostrak <pit.trak@gmail.com>
2022-04-18 13:32:28 +0300

    Add NewPayload func

--- token/payload.go
+++ token/payload.go
@@ -12,3 +12,19 @@ type Payload struct {
 	IssuedAt  time.Time `json:"issued_at"`
 	ExpiredAt time.Time `json:"expired_at"`
 }
+
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+
+	return payload, nil
+}
