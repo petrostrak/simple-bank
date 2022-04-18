f8cecf5d5d8496f1aa55990121ce546f80fa517b
petrostrak <pit.trak@gmail.com>
2022-04-18 14:55:55 +0300

    Add TestExpiredPasetoToken

--- token/paseto_test.go
+++ token/paseto_test.go
@@ -31,3 +31,17 @@ func TestPasetoMaker(t *testing.T) {
 	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
 	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
 }
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(util.RandomString(32))
+	require.NoError(t, err)
+
+	token, err := maker.CreateToken(util.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	payload, err := maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
